28bceae29653dcf2005a6249c852eb2823630725
michaelatimore <michaelatimore@gmail.com>
2024-09-26 14:55:49 -0400

    Refactored server.ts to deal with an error due to the route being imporperly defined.

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,15 @@
-/*************  ✨ Codeium Command 🌟  *************/
-/**
- * Import the express module from the node_modules directory.
- * The express module is used to create web applications.
- * The express function is a top-level function exported by the module.
- * The express function returns an instance of the express class.
- * The express class is used to create web applications.
- * The express class has methods and properties that can be used to create web applications.
- */
 import express from "express";
 
-/**
- * Create an instance of the express class.
- * The express instance is used to create web applications.
- * The express instance has methods and properties that can be used to create web applications.
- */
 const app = express();
 
-/**
- * Define a route for the root URL ('/').
- * The route will respond with a JSON object.
- * The JSON object will contain a property called 'serverMessage' with the value 'Hello World!'.
- */
+const PORT = process.env.PORT ?? 3000;
+
 app.get("/", (req, res) => {
-    res.json({serverMessage:"Hello World!"});
+  res.json({ serverMessage: "Hello world!" });
 });
 
-/**
- * Start the server.
- * The server will listen on port 3000.
- * When the server is started, a message will be logged to the console.
- * The message will indicate that the server has started and is listening on port 3000.
- */
-app.listen(3000, () => {
-    console.log("Server started on http://localhost:3000");
-})
+app.listen(PORT, () => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(`server running at http://localhost:${PORT}`);
+  }
+});
