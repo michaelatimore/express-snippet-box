2c9818ca1f58ee334befe234e9453e4ee19a6470
michaelatimore <michaelatimore@gmail.com>
2024-10-01 02:41:23 -0400

    Practiced connecting to the database and copying an sql file into my containers temp folder

--- initialize.sql
+++ initialize.sql
@@ -1,4 +1,4 @@
-create table if not exists users (
+/*create table if not exists users (
     id serial primary key,
     email text unique not null,
     first_name varchar(25) not null,
@@ -13,7 +13,31 @@ create table if not exists snippets (
     expiration_date timestamptz,
     snippet_owner integer not null,
     foreign key (snippet_owner) references users (id)
+);*/
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25)
 );
+
+CREATE TABLE IF NOT EXISTS snippets (
+    snippet_id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    title VARCHAR(256),
+    creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    expiration_date TIMESTAMPTZ,
+    snippet_owner INTEGER NOT NULL,
+    FOREIGN KEY (snippet_owner) REFERENCES users (id)
+);
+
+-- Insert some initial data
+INSERT INTO users (email, first_name, last_name) 
+VALUES ('john.doe@example.com', 'John', 'Doe');
+
+INSERT INTO snippets (content, title, expiration_date, snippet_owner) 
+VALUES ('This is a test snippet.', 'Test Snippet', '2025-01-01 00:00:00', 1);
 
 
 /*
