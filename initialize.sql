ce6f882a5de296457bcd990ef948825c8f67acbf
michaelatimore <michaelatimore@gmail.com>
2024-10-01 03:04:06 -0400

    Practiced connecting to the database and copying an sql file into my containers temp folder

--- initialize.sql
+++ initialize.sql
@@ -1,4 +1,7 @@
-/*create table if not exists users (
+/*
+ALL LOWERCASE KEYWORDS
+
+create table if not exists users (
     id serial primary key,
     email text unique not null,
     first_name varchar(25) not null,
@@ -24,20 +27,27 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS snippets (
     snippet_id SERIAL PRIMARY KEY,
-    content TEXT NOT NULL,
     title VARCHAR(256),
+    content TEXT NOT NULL,
     creation_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     expiration_date TIMESTAMPTZ,
     snippet_owner INTEGER NOT NULL,
     FOREIGN KEY (snippet_owner) REFERENCES users (id)
 );
 
--- Insert some initial data
-INSERT INTO users (email, first_name, last_name) 
-VALUES ('john.doe@example.com', 'John', 'Doe');
+/* Insert users*/
+INSERT INTO users (email, first_name, last_name) /*user ids are auto increment so they don't have to be inserted*/
+VALUES 
+('john.doe@example.com', 'John', 'Doe'),
+('jane.smith@example.com', 'Jane', 'Smith'),
+('bob.johnson@example.com', 'Bob', 'Johnson');
 
-INSERT INTO snippets (content, title, expiration_date, snippet_owner) 
-VALUES ('This is a test snippet.', 'Test Snippet', '2025-01-01 00:00:00', 1);
+/*Insert snippets*/
+
+INSERT INTO snippets (content, title, expiration_date, snippet_owner) /*current_date will be set by default to the current date so it's not neccessary to explicitly insert it.*/
+('Test Snippet 1', 'This is a test snippet.', '2025-01-01 00:00:00', 1),
+('Test Snippet 2', 'Another test snippet.', '2025-02-01 00:00:00', 2),
+('Test Snippet 3', 'A third test snippet.', '2025-03-01 00:00:00', 3);
 
 
 /*
