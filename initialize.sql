8eb60865850ecfdf4b573e96901e4a519c776863
michaelatimore <michaelatimore@gmail.com>
2024-10-03 18:15:50 -0400

    Decided on the Unix value for generating the dates

--- initialize.sql
+++ initialize.sql
@@ -2,18 +2,17 @@ create table if not exists users (
     id serial primary key,
     email text unique not null,
     first_name varchar(25) not null,
-    last_name varchar(25)
+    last_name varchar(25) not null
 );
 
 create table if not exists snippets (
     snippet_id uuid default gen_random_uuid() primary key not null,
-    title varchar(256),
+    title varchar(256) not null,
     content text not null,
     creation_date int default extract (epoch from now()) not null,
     expiration_date int default extract (epoch from now() + interval '1 month') not null,
-    snippet_owner integer not null,
+    snippet_owner int not null,
     foreign key (snippet_owner) references users (id)
-
 );
 
 
