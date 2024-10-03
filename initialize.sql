f8d65fa38c95b6bc88f0829c13c88f2d69d34caa
michaelatimore <michaelatimore@gmail.com>
2024-10-03 15:55:59 -0400

    Edited .sql file to utilize Unix values for the table dates

--- initialize.sql
+++ initialize.sql
@@ -6,13 +6,18 @@ create table if not exists users (
 );
 
 create table if not exists snippets (
-    snippet_id uuid default gen_random_uuid() primary key,
+    snippet_id uuid default gen_random_uuid() primary key not null,
     title varchar(256),
     content text not null,
-    creation_date timestamp default now(),
-    expiration_date timestamp,
-    snippet_owner integer,
+    creation_date int default extract (epoch from now()) not null,
+    expiration_date int default extract (epoch from now() + interval '1 month') not null,
+    snippet_owner integer not null,
     foreign key (snippet_owner) references users (id)
 
 );
 
+
+
+       creation_date timestamp default now(),
+    expiration_date timestamp,
+  
